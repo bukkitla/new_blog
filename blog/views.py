0572582abf0943f574808e7b0e64b99e42b9ca13
bukkitla <brshankar95@gmail.com>
2020-05-01 12:26:26 +0530

    created dummy data for posts

--- blog/views.py
+++ blog/views.py
@@ -2,9 +2,23 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
+posts=[
+    {'author':"ravi",
+    'title':'Blog Post1',
+     'content':'First Post',
+     'date_posted':'27 AUG 2018'
+     },
+{'author':"Shankar",
+    'title':'Blog Post2',
+     'content':'Second Post',
+     'date_posted':'28 AUG 2018'
+     }
+]
+
+context={'posts':posts}
 
 def home(request):
-    return render(request,'blogs/home.html')
+    return render(request,'blog/home.html',context=context)
 
 def about(request):
-    return render(request,'blogs/home.html')
+    return render(request,'blog/home.html')
